fa5143947d298b3704ffede633e04d285423aa6b
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2021-09-16 11:02:01 +0530

    DM changes

--- DM/files/q3.py
+++ DM/files/q3.py
@@ -10,13 +10,9 @@ Created on Wed Sep  8 22:32:45 2021
 
 
 import pandas as pd
-import numpy as np
-
-import os
-os.chdir(r'/home/pranshu/Desktop/SEM1-Downloads/DM/files/')
 
 
-df3 = pd.read_csv( "data/districts.csv", low_memory=False)
+df3 = pd.read_csv( "districts.csv", low_memory=False)
 
 tbd = [ 'Date', 'District','Confirmed']
 df = df3[tbd]
@@ -71,7 +67,6 @@ df3.dropna(subset=['Date'], inplace=True)
 
 df3.reset_index(inplace=True, drop=True)
 
-df3['cases'] = df3['Confirmed'].astype(int)
 
 df3.reset_index(drop=True, inplace=True)
 
@@ -79,6 +74,33 @@ df3.reset_index(drop=True, inplace=True)
 #f3['Week_Number'] = df3['Date Announced'].dt.week
 # Getting year. Weeknum is common across years to we need to create unique index by using year and weeknum
 #df3['Year'] = df3['Date Announced'].dt.year
+
+
+
+
+df3.sort_values(['District','Date'], inplace = True)
+
+
+
+
+imd = []
+districts = list(df3['District'].unique() )
+
+
+for dist in districts:
+    tmp = df3[df3['District']==dist].copy()
+    tmp.sort_values('Date', inplace = True)
+    tmp.reset_index(drop=True, inplace=True)
+    for i in range(len(tmp)-1,0,-1):
+        tmp.loc[i, 'Confirmed'] = tmp.loc[i, 'Confirmed'] - tmp.loc[i-1, 'Confirmed']
+    
+    imd.append(tmp)  
+
+
+df3 = pd.concat(imd, ignore_index=True)
+
+
+
 
 
 df3 = df3.set_index(pd.to_datetime(df3['Date']))
@@ -125,17 +147,18 @@ final['weekid'] = final['weekid'].apply(lambda x: x+1)
 
 
 cols = list(final.columns)
-final.drop('Confirmed', inplace = True, axis =1)
-final = final [[ 'weekid', 'district','cases']]
+final = final [['district','weekid', 'Confirmed']]
 
-final.columns = [ 'weekid', 'District','cases']
+final.columns = [ 'districtid', 'weekid','cases']
 
 final.reset_index(drop = True, inplace=True)
 
+dcodes.columns = ['districtid', 'District_Key']
 
-final = pd.merge(final,  dcodes, on = 'District')
 
-final.drop('District', inplace = True, axis =1)
+final = pd.merge(final,  dcodes, on = 'districtid')
+
+final.drop('districtid', inplace = True, axis =1)
 final = final[ [ 'District_Key','weekid','cases' ]]
 
 final.columns = [ 'districtid','weekid','cases']
@@ -144,29 +167,31 @@ final.columns = [ 'districtid','weekid','cases']
 final.sort_values(['districtid', 'weekid'], inplace = True)
 
 
-
-all_cities = final.districtid.unique()
-
-out =[]
-
-for city in all_cities:
-    tmp = final[final['districtid']==city].copy()
-    tmp.sort_values(['districtid', 'weekid'], inplace = True)
-    tmp.reset_index(inplace=True, drop=True)
-    #print(tmp)
-    for i in range(len(tmp)-3, 1, -1):
-        #tmp.loc[]
-        tmp.loc[i, 'cases'] -= tmp.loc[i-1, 'cases']
-       # pass
-    tmp.drop([len(tmp)-2, len(tmp)-1], inplace = True, axis = 0)
-    
-    #print(tmp)
-    out.append(tmp)
-    
-
-    
-
-final = pd.concat(out, ignore_index=True)
+# =============================================================================
+# 
+# all_cities = final.districtid.unique()
+# 
+# out =[]
+# 
+# for city in all_cities:
+#     tmp = final[final['districtid']==city].copy()
+#     tmp.sort_values(['districtid', 'weekid'], inplace = True)
+#     tmp.reset_index(inplace=True, drop=True)
+#     #print(tmp)
+#     for i in range(len(tmp)-3, 1, -1):
+#         #tmp.loc[]
+#         tmp.loc[i, 'cases'] -= tmp.loc[i-1, 'cases']
+#        # pass
+#     tmp.drop([len(tmp)-2, len(tmp)-1], inplace = True, axis = 0)
+#     
+#     #print(tmp)
+#     out.append(tmp)
+#     
+# 
+#     
+# 
+# final = pd.concat(out, ignore_index=True)
+# =============================================================================
 final.to_csv('output/week-cases-time.csv', index = False)
 
 
@@ -178,7 +203,7 @@ final.to_csv('output/week-cases-time.csv', index = False)
 # =============================================================================
 
 
-df3 = pd.read_csv( "data/districts.csv", low_memory=False)
+df3 = pd.read_csv( "districts.csv", low_memory=False)
 
 tbd = [ 'Date', 'District','Confirmed']
 df = df3[tbd]
@@ -220,6 +245,43 @@ df3 = df3[~(df3['Date'] < '2020-03-15')]
 df3 = df3[~(df3['Date'] > '2021-08-14')]
 
 
+df3['Confirmed'].fillna(0, inplace=True)
+
+
+df3.dropna(subset=['Date'], inplace=True)
+
+df3.reset_index(inplace=True, drop=True)
+
+
+df3.reset_index(drop=True, inplace=True)
+
+
+#f3['Week_Number'] = df3['Date Announced'].dt.week
+# Getting year. Weeknum is common across years to we need to create unique index by using year and weeknum
+#df3['Year'] = df3['Date Announced'].dt.year
+
+
+
+
+df3.sort_values(['District','Date'], inplace = True)
+
+imd = []
+districts = list(df3['District'].unique() )
+
+
+for dist in districts:
+    tmp = df3[df3['District']==dist].copy()
+    tmp.sort_values('Date', inplace = True)
+    tmp.reset_index(drop=True, inplace=True)
+    for i in range(len(tmp)-1,0,-1):
+        tmp.loc[i, 'Confirmed'] = tmp.loc[i, 'Confirmed'] - tmp.loc[i-1, 'Confirmed']
+    
+    imd.append(tmp)  
+
+
+df3 = pd.concat(imd, ignore_index=True)
+
+
 for i in range(4,9):
     df3.loc[((df3['Date'] >= '2020-0'+str(i)+'-15') & (df3['Date'] <= '2020-0'+str(i+1)+'-14')), 'monthid'] = i-3
 
@@ -238,38 +300,71 @@ for i in range(1,8):
 
 df3['monthid'] = df3['monthid'].astype(int)
 
-tmp = df3[df3['District']=='Anantapur']
 df3.drop('Date', inplace = True, axis = 1)
 
+ 
+ 
+ 
+df3 = df3.groupby(['District', 'monthid']).agg('sum')
+# 
+df3['districtid'] = df3.index
+df3['monthid'] = df3['districtid'].apply(lambda x: x[1])
+df3['districtid'] = df3['districtid'].apply(lambda x: x[0])
 
-all_cities = df3.District.unique()
 
-out =[]
+df3.reset_index(drop = True, inplace=True)
 
-for city in all_cities:
-    
-    tmp = df3[df3['District']==city].copy()
-    tmp.sort_values(['District', 'monthid'], inplace = True)
-    tmp.reset_index(inplace=True, drop=True)
-    #print(tmp)
-    
-    for i in range(1,17):
-        t = {}
-        try:
-            
-            t['districtid'] = city
-            t['monthid'] = i
-            foo = tmp[tmp['monthid']==i].copy()
-            foo.reset_index(inplace=True, drop=True)
-            t['cases'] = foo.loc[len(foo)-2, 'Confirmed'] - foo.loc[0, 'Confirmed']
-            
-        except:
-            t['districtid'] = city
-            t['monthid'] = i
-            t['cases'] = 0
-            print(city)
-            
-        out.append(t)
+df3 = df3[ [ 'districtid', 'monthid', 'Confirmed']]
+
+df3.columns = [ 'districtid', 'monthid', 'cases']
+
+dcodes.columns = ['districtid', 'District_Key']
+
+df3 = pd.merge(df3,  dcodes, on = 'districtid')
+df3.drop('districtid', inplace = True, axis = 1)
+
+
+df3 = df3[ [ 'District_Key','monthid','cases' ]]
+
+df3.columns = [ 'districtid','monthid','cases']
+
+df3.sort_values(['districtid', 'monthid'], inplace = True)
+
+
+
+df3.to_csv('output/month-cases-time.csv', index = False)
+
+# =============================================================================
+# 
+# all_cities = df3.District.unique()
+# 
+# out =[]
+# 
+# for city in all_cities:
+#     
+#     tmp = df3[df3['District']==city].copy()
+#     tmp.sort_values(['District', 'monthid'], inplace = True)
+#     tmp.reset_index(inplace=True, drop=True)
+#     #print(tmp)
+#     
+#     for i in range(1,17):
+#         t = {}
+#         try:
+#             
+#             t['districtid'] = city
+#             t['monthid'] = i
+#             foo = tmp[tmp['monthid']==i].copy()
+#             foo.reset_index(inplace=True, drop=True)
+#             t['cases'] = foo.loc[len(foo)-2, 'Confirmed'] - foo.loc[0, 'Confirmed']
+#             
+#         except:
+#             t['districtid'] = city
+#             t['monthid'] = i
+#             t['cases'] = 0
+#             print(city)
+#             
+#         out.append(t)
+# =============================================================================
     
 # =============================================================================
 # df3 = pd.concat(out, ignore_index=True)
@@ -293,18 +388,6 @@ for city in all_cities:
 # df3 = pd.merge(df3,  dcodes, on = 'districtid')
 # df3.drop('districtid', inplace = True, axis = 1)
 # =============================================================================
-
-df3 = pd.DataFrame(out)
-
-
-
-df3.sort_values(['districtid', 'monthid'], inplace = True)
-
-
-
-df3.to_csv('output/month-cases-time.csv', index = False)
-
-
 
 
 
